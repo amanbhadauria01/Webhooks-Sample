a5d077cefa2e617dfb9059d8aef2387c737cbc53
Aman <amanbhadauria01@gmail.com>
2022-02-24 20:12:07 +0530

    updates

--- app.js
+++ app.js
@@ -24,31 +24,6 @@ app.get('/', function (req, res) {
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
-
-app.get("/webhook", (req, res) => {
-  // Parse the query params
-  console.log("Got /webhook");
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
-
-  // Check if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === "subscribe" && token === config.verifyToken) {
-      // Responds with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  } else {
-    console.warn("Got /webhook but without needed parameters.");
-  }
-});
-
-
 app.get(['/facebook', '/instagram'], function (req, res) {
   if (
     req.query['hub.mode'] == 'subscribe' &&
