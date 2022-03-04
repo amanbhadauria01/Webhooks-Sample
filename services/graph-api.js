5144f83d94c287c75e3cac920cff382cc268be1c
Aman <amanbhadauria01@gmail.com>
2022-03-04 11:30:45 +0530

    can do pagination now, comments and their replies are also returned now

--- services/graph-api.js
+++ services/graph-api.js
@@ -10,20 +10,26 @@ module.exports = class GraphApi {
 
   // given a comment, check if it contains obscenity, if yes then delete this comment
   static async moderateComments_of_ig_comment(media_comment) {
-    console.log(media_comment.text);
-    if (media_comment.text.includes("Hi")) {
-      // delete media_comment
-      let url = new URL(`https://graph.facebook.com/${media_comment.id}?access_token=${config.userAccessToken}`);
-      let response = await fetch(url, {
-        method: "DELETE"
-      });
-      console.log("deleting comment");
-    }
+    // if (media_comment.text.includes("Hi")) {
+    //   // delete media_comment
+    //   let url = new URL(`https://graph.facebook.com/${media_comment.id}?access_token=${config.userAccessToken}`);
+    //   let response = await fetch(url, {
+    //     method: "DELETE"
+    //   });
+    //   console.log("deleting comment");
+    // }
+    // if (media_comment.hasOwnProperty('replies')){
+    //   // code will not handle more than 25 replies
+    //   let media_comments = media_comment.replies.data;
+    //   for (const media_comment of media_comments) {
+    //     this.moderateComments_of_ig_comment(media_comment);
+    //   } 
+    // }
   }
   // given ig_media, check all the comments of that ig_media
   static async moderateComments_of_ig_media(ig_media) {
     console.log("checking comments of " + ig_media.id);
-    let url = new URL(`https://graph.facebook.com/${ig_media.id}/comments?access_token=${config.userAccessToken}`);
+    let url = new URL(`https://graph.facebook.com/v13.0/${ig_media.id}/comments?pretty=0&fields=text%2Ctimestamp%2Creplies&limit=50&access_token=${config.userAccessToken}`)
     while (true) {
       let response = await fetch(url)
         .then(async (res) => {
@@ -36,6 +42,7 @@ module.exports = class GraphApi {
         this.moderateComments_of_ig_comment(media_comment);
       }
       if(response.hasOwnProperty('paging')){
+        url = new URL(`https://graph.facebook.com/v13.0/${ig_media.id}/comments?pretty=0&fields=text%2Ctimestamp%2Creplies&limit=50&after=${response.paging.cursors.after}&access_token=${config.userAccessToken}`)
         url = new URL(response.paging.next);
       }else{
         break;
